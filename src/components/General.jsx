e5c88235cb1e7d6475e3dca1363c69c3e0361304
CheesyChank <charleszhang418@hotmail.com>
2024-06-24 23:28:02 -0700

    Fix mobile side

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -6,13 +6,13 @@ const TechStack = () => {
     frontend: ["HTML", "CSS", "JavaScript", "Next.js", "React", "Tailwind", "TypeScript"],
     backend: ["AWS", "Bash", "C/C++", "Docker", "FastAPI", "Flask", "Go", "Java", 
             "MongoDB", "Node.js", "REST API", "SQL", "Spring Boot"],
-    ml: ["Python", "Matplotlib", "NumPy", "OpenCV", "Pandas", "PyTorch", "Scikit-Learn"]
+    ML: ["Python", "Matplotlib", "NumPy", "OpenCV", "Pandas", "PyTorch", "Scikit-Learn"]
 
   };
 
   return (
     <Section title="Tech Stack">
-      <div className="flex flex-row justify-center items-start gap-8 mx-auto">
+      <div className="flex flex-col justify-center items-start gap-8 mx-auto">
         {Object.keys(stacks).map((key) => (
           <div key={key} className="flex-1 bg-white dark:bg-slate-900 p-6 ring-1 ring-slate-900/5 rounded-xl shadow-md dark:shadow-gray-300 flex flex-col items-center gap-4">
             <h3 className="text-2xl font-semibold text-center">
