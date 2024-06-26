b5c46aa6a5645fbed5487b50bfbed602e87909f0
CheesyChank <charleszhang418@hotmail.com>
2024-06-25 20:52:37 -0700

    Update General.jsx

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -12,13 +12,13 @@ const TechStack = () => {
 
   return (
     <Section title="Tech Stack">
-      <div className="flex flex-col justify-center items-start gap-8 mx-auto">
+      <div className="flex justify-center items-start gap-8 mx-auto">
         {Object.keys(stacks).map((key) => (
           <div key={key} className="flex-1 bg-white dark:bg-slate-900 p-6 ring-1 ring-slate-900/5 rounded-xl shadow-md dark:shadow-gray-300 flex flex-col items-center gap-4">
             <h3 className="text-2xl font-semibold text-center">
               {key.charAt(0).toUpperCase() + key.slice(1)}
             </h3>
-            <ul className="grid grid-cols-2 gap-3">
+            <ul className="grid grid-cols-3 gap-3">
               {stacks[key].map((tech) => (
                 <li key={tech} className="p-2 bg-gray-100 dark:bg-gray-800 rounded-md text-center">
                   {tech}
